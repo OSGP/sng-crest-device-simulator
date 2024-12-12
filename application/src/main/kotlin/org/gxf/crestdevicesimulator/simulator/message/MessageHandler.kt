cc02b75874dc707b61adc4ed23f04f0d983198ff
Sander Verbruggen <sander.verbruggen@alliander.com>
2024-12-12 16:43:40 +0100

    FDP-2826: clean up
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdevicesimulator/simulator/message/MessageHandler.kt
+++ application/src/main/kotlin/org/gxf/crestdevicesimulator/simulator/message/MessageHandler.kt
@@ -25,7 +25,6 @@ class MessageHandler(
     private val coapClientService: CoapClientService,
     private val simulatorProperties: SimulatorProperties,
     private val pskService: PskService,
-    private val mapper: ObjectMapper,
     private val commandService: CommandService,
     private val handlers: MutableList<out CommandHandler>,
     private val jacksonObjectMapper: ObjectMapper
@@ -33,11 +32,9 @@ class MessageHandler(
     private val logger = KotlinLogging.logger {}
 
     companion object {
-        private const val URC_FIELD = "URC"
         private const val URC_PSK_SUCCESS = "PSK:SET"
         private const val URC_PSK_ERROR = "PSK:EQER"
         private const val REBOOT_SUCCESS = "INIT"
-        private const val DL_FIELD = "DL"
     }
 
     fun sendMessage(simulatorState: SimulatorState, sendShortMessage: Boolean = false): Boolean {
